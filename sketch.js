8ee72518c220c547b6bf2e5249fa6c6c675ad8e5
YamiAtem <63883108+YamiAtem@users.noreply.github.com>
2020-09-30 07:30:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -87,7 +87,7 @@ function draw() {
     score = score + Math.round(getFrameRate()/60);
     ground.velocityX = -(6 + 3*score/100);
   
-    if(touches.length > 0 || keyDown("space") && trex.y > 161) {
+    if(keyDown("space") && trex.y > 161) {
       trex.velocityY = -12;
       jumpSound.play();
     }
